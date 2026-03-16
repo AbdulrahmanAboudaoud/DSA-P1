eb8ef2a12b74e829496f652c755428274d9b7241
Ibrahim <99918097+ABID2001@users.noreply.github.com>
2026-03-16 01:45:10 +0100

    generic arrays sprint 1

--- DSA-P1-KH/Program.cs
+++ DSA-P1-KH/Program.cs
@@ -6,7 +6,7 @@ namespace DSA_P1_KH;
 
 class Program
 {
-    static void Main()
+    public static void Main()
     {
         string filePath = Path.Combine(
              AppContext.BaseDirectory,
